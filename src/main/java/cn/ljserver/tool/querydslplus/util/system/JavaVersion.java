7d7d1c8f0dfd21e52a2b98624ac6de8391efbccf
mose <602187256>
2025-01-24 21:16:55 +0800

    update 17

--- src/main/java/cn/ljserver/tool/querydslplus/util/system/JavaVersion.java
+++ src/main/java/cn/ljserver/tool/querydslplus/util/system/JavaVersion.java
@@ -45,16 +45,81 @@ public enum JavaVersion {
     /**
      * Java 1.8.
      */
-    JAVA_1_8(1.8f, "1.8");
+    JAVA_1_8(1.8f, "1.8"),
+
+    /**
+     * Java 9.
+     */
+    JAVA_9(9, "9"),
+
+    /**
+     * Java 10.
+     */
+    JAVA_10(10, "10"),
+
+    /**
+     * Java 11.
+     */
+    JAVA_11(11, "11"),
+
+    /**
+     * Java 12.
+     */
+    JAVA_12(12, "12"),
+
+    /**
+     * Java 13.
+     */
+    JAVA_13(13, "13"),
+
+    /**
+     * Java 14.
+     */
+    JAVA_14(14, "14"),
+
+    /**
+     * Java 15.
+     */
+    JAVA_15(15, "15"),
+
+    /**
+     * Java 16.
+     */
+    JAVA_16(16, "16"),
+
+    /**
+     * Java 17.
+     */
+    JAVA_17(17, "17"),
+
+    /**
+     * Java 18.
+     */
+    JAVA_18(18, "18"),
+
+    /**
+     * Java 19.
+     */
+    JAVA_19(19, "19"),
+
+    /**
+     * Java 20.
+     */
+    JAVA_20(20, "20"),
+
+    /**
+     * Java 21.
+     */
+    JAVA_21(21, "21");
 
     /**
      * The float value.
      */
-    private float value;
+    private final float value;
     /**
      * The standard name.
      */
-    private String name;
+    private final String name;
 
     /**
      * Constructor.
@@ -97,6 +162,32 @@ public enum JavaVersion {
             return JAVA_1_7;
         } else if ("1.8".equals(nom)) {
             return JAVA_1_8;
+        } else if ("9".equals(nom)) {
+            return JAVA_9;
+        } else if ("10".equals(nom)) {
+            return JAVA_10;
+        } else if ("11".equals(nom)) {
+            return JAVA_11;
+        } else if ("12".equals(nom)) {
+            return JAVA_12;
+        } else if ("13".equals(nom)) {
+            return JAVA_13;
+        } else if ("14".equals(nom)) {
+            return JAVA_14;
+        } else if ("15".equals(nom)) {
+            return JAVA_15;
+        } else if ("16".equals(nom)) {
+            return JAVA_16;
+        } else if ("17".equals(nom)) {
+            return JAVA_17;
+        } else if ("18".equals(nom)) {
+            return JAVA_18;
+        } else if ("19".equals(nom)) {
+            return JAVA_19;
+        } else if ("20".equals(nom)) {
+            return JAVA_20;
+        } else if ("21".equals(nom)) {
+            return JAVA_21;
         } else {
             return null;
         }
